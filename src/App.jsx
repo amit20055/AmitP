122dd8c64ec2fb964dca486dbc77211d9c865b72
Amit Prajapati <amitprajapati@gmail.com>
2026-01-14 20:29:39 +0530

    Fix imports and remove main91

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useDispatch } from 'react-redux'
+import './App.css'
+import Header from "./Header91.jsx";
+import Product from "./Product91.jsx";
 
-import Main91 from "./main91"
-
-function App() {
-  
-
+import { clearAllItems } from './redux/slice';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CartList from './CartList';
+function Main91() {
+const dispatch=useDispatch();
   return (
     <>
-    <Main91/>
-    
+<BrowserRouter>
+ <Header />
+
+ <Routes>
+<Route path="/" element={ <Product />}></Route>
+<Route path="/cart" element={<CartList />}></Route>
+
+ </Routes>
+</BrowserRouter>
     </>
   )
 }
 
-export default App
+export default Main91;
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import Main91 from "./main91";
+import Main91 from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
